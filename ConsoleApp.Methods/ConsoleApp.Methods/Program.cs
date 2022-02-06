9889127b926a6174da695521ae4beb62a12ae9d8
Mammadli999 <abbasfm@code.edu.az>
2022-02-06 18:58:08 +0400

    Methods

---
+++ ConsoleApp.Methods/ConsoleApp.Methods/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp.Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SayHello("Aqil","Abbasov",22);
+            //SayHello("Akif","Abisov",33);
+
+            //string fullText = $"Salam,Aqil Abbasov / 27 yash!";
+
+
+            //Console.WriteLine(fullText);
+            l1:
+            Console.Write("Eded:");
+            string aStr = Console.ReadLine();
+            double a;
+            try
+            {
+                a = Convert.ToDouble(aStr);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Yeniden Eded Daxil Et");
+                goto l1;
+            }
+
+            double c = Power(a);
+
+            Console.WriteLine(c);
+
+            Console.Title = $"Result: {c}";
+
+            Console.ReadKey();
+        }
+
+        static double Power (double a)
+        {
+            double cavab = a * a;
+            return cavab;
+        }
+
+        //static string SayHello(string name,string surname,int age)
+        //{
+        //   // Console.WriteLine($"Salam,{name} {surname} / {age} yash!");
+
+        //    string fullText = $"Salam,{name} {surname} / {age} yash!";
+            
+
+        //    return fullText;
+        //}
+       
+    }
+}
